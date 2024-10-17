86c66d97623a3201affaa29ad827620dfa6e3896
Hanshal Mehta <122217807+hanshal101@users.noreply.github.com>
2024-10-17 16:22:19 +0000

    adding basic container lifecycle
    Signed-off-by: hanshal101 <122217807+hanshal101@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 
@@ -64,4 +66,57 @@ func main() {
 		Role:   "worker",
 	}
 	fmt.Printf("node: %v\n", n)
+
+	fmt.Println("creating a container")
+	dockerTask, createTask := createContainer()
+	if createTask.Error != nil {
+		fmt.Printf(createTask.Error.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Sleeping for 10 Seconds")
+	time.Sleep(10 * time.Second)
+
+	rmTask := stopContainer(dockerTask, createTask.ContainerID)
+	if rmTask.Error != nil {
+		fmt.Printf(rmTask.Error.Error())
+		os.Exit(1)
+	}
+}
+
+// testing contatiner start & stop
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "container-1",
+		Image: "postgres:alpine",
+		Env: []string{
+			"POSTGRES_USER=core",
+			"POSTGRES_PASSWORD=core",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerID, d.Config)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+	result := d.Stop(id)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+	fmt.Printf("Container %s is stopped with config %v\n", result.ContainerID, d.Config)
+	return &result
+
 }
--- task/task.go
+++ task/task.go
@@ -1,8 +1,16 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"os"
 	"time"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 )
@@ -43,4 +51,121 @@ type TaskEvent struct {
 	State     State
 	Timestamp time.Time
 	Task      Task
+}
+
+// model to run a container will sufficient configuration
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
+
+// the docker model with the docker client and th	 configuration of the container to run
+type Docker struct {
+	Client      *client.Client
+	Config      Config
+	ContainerID string
+}
+
+// this will be used as a result after the task is assigned to analyze whether the docker container of executed sucessfully or not
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerID string
+	Result      string
+}
+
+// This is similiar to docker run, stop, rm command
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(
+		ctx, d.Config.Image, image.PullOptions{},
+	)
+	if err != nil {
+		log.Printf("Error in pulling the image %s: %v\n", d.Config, err)
+		return DockerResult{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy{
+		Name: container.RestartPolicyMode(d.Config.RestartPolicy),
+	}
+
+	r := container.Resources{
+		Memory: d.Config.Memory,
+	}
+
+	cc := container.Config{
+		Image: d.Config.Image,
+		Env:   d.Config.Env,
+	}
+
+	hc := container.HostConfig{
+		RestartPolicy:   rp,
+		Resources:       r,
+		PublishAllPorts: true,
+	}
+
+	resp, err := d.Client.ContainerCreate(
+		ctx, &cc, &hc, nil, nil, d.Config.Name,
+	)
+	if err != nil {
+		log.Printf("Error in creating container %s: %v", d.Config, err)
+		return DockerResult{Error: err}
+	}
+
+	if err := d.Client.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		log.Printf("Error in starting the container %s: %v", d.Config, err)
+		return DockerResult{Error: err}
+	}
+
+	d.ContainerID = resp.ID
+
+	out, err := d.Client.ContainerLogs(
+		ctx, resp.ID, container.LogsOptions{ShowStdout: true, ShowStderr: true},
+	)
+	if err != nil {
+		log.Printf("Error in getting container logs %s: %v", d.Config, err)
+		return DockerResult{Error: err}
+	}
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	return DockerResult{
+		Error:       nil,
+		Action:      "start",
+		ContainerID: resp.ID,
+		Result:      "success",
+	}
+}
+
+func (d *Docker) Stop(id string) DockerResult {
+	log.Printf("Attempting to stop container: %s", id)
+	ctx := context.Background()
+	if err := d.Client.ContainerStop(
+		ctx, id, container.StopOptions{},
+	); err != nil {
+		log.Printf("Error in stopping container %s: %v", d.Config, err)
+		return DockerResult{Error: err}
+	}
+
+	if err := d.Client.ContainerRemove(
+		ctx, id, container.RemoveOptions{},
+	); err != nil {
+		log.Printf("Error in removing container %s: %v", d.Config, err)
+		return DockerResult{Error: err}
+	}
+
+	return DockerResult{
+		Error:       nil,
+		Action:      "stop",
+		ContainerID: id,
+		Result:      "success",
+	}
 }
